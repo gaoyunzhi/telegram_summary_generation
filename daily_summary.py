3042b3e96b9773c5d6ff0980a51eda58a509cb21
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-08 16:14:33 -0700

    commit

--- daily_summary.py
+++ daily_summary.py
@@ -9,42 +9,20 @@ from telegram_util import log_on_fail
 from telegram.ext import Updater
 import cached_url
 from message import Message
+import sys
 
-with open('credential') as f:
-    bot = Updater(yaml.load(f, Loader=yaml.FullLoader)['bot_token'], 
-        use_context=True).bot
+def getFile(name):
+    with open(name) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+bot = getFile('credential')['bot_token'], use_context=True).bot
+debug_group = bot.get_chat(-1001198682178)
 
 last_run = 0
 
 def readPool():
     with open('pool') as f:
         return [x.strip() for x in f.read().split() if x.strip()]
-
-
-with open('DB') as f:
-    DB = yaml.load(f, Loader=yaml.FullLoader)
-
-def getParsedText(text):
-    result = ''
-    for item in text:
-        if item.name in set(['br']):
-            result += '\n'
-            continue
-        if item.name == 'i':
-            if item.text:
-                result += '<i>' + item.text + '</i>'
-            continue
-        if item.name == 'a':
-            telegraph_url = export_to_telegraph.export(item['href'])
-            if telegraph_url:
-                item['href'] = telegraph_url
-                del item['rel']
-                if 'http' in item.text:
-                    item.contents[0].replaceWith(telegraph_url)
-        if str(item).startswith('原文') and 'telegra' in result:
-            return result
-        result += str(item)
-    return result
 
 def keyMatch(chat_id, author, result):
     if (not isinstance(chat_id, int)) or (not DB[chat_id]):
@@ -60,24 +38,42 @@ def intersect(l1, l2):
 def getSoup(name):
     return BeautifulSoup(cached_url.get('https://telete.in/s/' + name), 'html.parser')
 
+def getRawList(messages, setting):
+    for msg in messages.items.values():
+        if msg.match(keys):
+            raw_list.append([msg.getWeight(), msg.getText(setting)])
+    raw_list.sort(reverse=True)
+    if 'test' in sys.argv:
+        if len(raw_list) > 10 or not raw_list:
+            print('warning, %s matched %d item' % (name, len(raw_list)))
+    return [y for x, y in raw_list[:10]]
+
 def getMessages():
+    messages = {}
     for name in readPool():
         soup = getSoup(name)
         for msg in soup.find_all('div', class_='tgme_widget_message'):
             msg = Message(msg)
-            if not msg.text:
-                continue
-            text = msg.find('div', class_='tgme_widget_message_text')
-            if (not text) or (not text.text):
-                continue
-            hash_value = hashlib.sha224(str(text.text).encode('utf-8')).hexdigest()
-            if hash_value in hashes:
-                continue
-            author = msg.find('div', class_='tgme_widget_message_author')
-            result = getParsedText(text)
+            if msg.getTitle():
+                messages[msg.getID()] = msg
+    settings = getFile('setting')
+    for name, keys in getFile('subscription').items():
+        print(name, keys)
+        setting = settings[name]
+        raw_list = getRawList(messages, setting)
+        if not raw_list:
+            continue
+        if setting == 'cn':
+            bot.send_message('@' + name, disable_web_page_preview=True)        
+
+
+
+
+            
 
 @log_on_fail(debug_group)
 def loopImp():
+    global last_run
     if time.time() - last_run < 20 * 60 * 60:
         return
     last_run = time.time()
--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import cached_url
 import sys
-from telegram_util import cleanUrl
+from telegram_util import cleanUrl, matchKeys
 
 def getCnLink(item):
 	if 'telegra.ph' not in item['href']:
@@ -21,35 +21,57 @@ def getTextCN(soup):
 		x.replace_with(' %s ' % getCnLink(x))
 	return cleanUrl(x.text)
 
+# TODO: may need timestamp info
 class Message():
 	def __init__(self, soup):
-		self.raw_text = soup.find('div', class_='tgme_widget_message_text')
+		self.soup = soup
+		self.raw_text = soup.find('div', class_='tgme_widget_message_text') \
+			or BeautifulSoup('', features='lxml')
 		self.text_cn = '' # 墙内版本
 		
 	def getCnText(self):
 		self.text_cn = self.text_cn or getTextCN(self.raw_text)
 		return self.text_cn
 
+	def getText(self, locale):
+		prefix = '【%s】\n' % self.getTitle() 
+		if locale == 'cn':
+			return prefix + self.getCnText().replace('\n\n', '\n')
+		# May need to deal with '<br>'
+		return prefix + str(self.raw_text)
+
 	def getMsgPreview(self):
-		preview = self.raw_text.find('a', class_='tgme_widget_message_link_preview')
+		preview = self.soup.find('a', class_='tgme_widget_message_link_preview')
 		if preview:
 			return preview.text
+
+	def getTitle(self):
+		title = self.soup.find('div', class_='link_preview_title')
+		if title:
+			return title.text
 
 	def getHiddenText(self):
 		raw = [self.getOrgLink(), self.getMsgLink(), self.getMsgPreview()]
 		return '\n\n'.join([x or '' for x in raw])
 
 	def getView(self):
-		return int(self.raw_text.find('span', class_=tgme_widget_message_views).text)
+		return int(self.soup.find('span', class_=tgme_widget_message_views).text)
+
+	def getWeight(self):
+		return self.getView + len(self.raw_text.text) * 10
 		
 	def getOrgLink(self):
-		forward = self.raw_text.find('a', class_='tgme_widget_message_forwarded_from_name')
+		forward = self.soup.find('a', class_='tgme_widget_message_forwarded_from_name')
 		if forward:
 			return forward['href']
 
 	def getMsgLink(self):
-		return self.raw_text.find('a', class_='tgme_widget_message_date')['href']
+		return self.soup.find('a', class_='tgme_widget_message_date')['href']
 
 	def getID(self):
 		return self.getOrgLink() or self.getMsgLink()
+
+	def match(self, keys):
+		return matchKeys(self.getHiddenText(), keys) or \
+			matchKeys(str(self.raw_text), keys)
 
