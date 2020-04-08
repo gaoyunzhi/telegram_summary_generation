2866aab7b3b689eec40e8688e3b414cacb200b69
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-08 14:29:32 -0700

    commit

--- README.md
+++ README.md
@@ -1,42 +1,5 @@
-# telegram_channel_subscription_v2_bot
+# telegram_summary_generation
 
-Subscribe messages from public channels. 
+subscribe message from a list of channels, and send daily summary to destination group/channel.
 
-You are also welcome to use my [@channel_subscription_v2_bot](https://t.me/channel_subscription_v2_bot) without create your own bot.
-
-Feedback / feature requests are welcomed at [@dushufenxiang_chat](https://t.me/dushufenxiang_chat).
-
-## how to install
-
-Talk with the [BotFather](https://t.me/botfather) and ask it for a bot.
-
-Once you get the token, run `python3 setup.py`.
-
-The bot will automatically be running at the end of setup.
-
-## feature
-
-Subscribe by key words.
-Subscription pool is shared.
-
-## bot description
-
-Subscribe messages from public channels. 
-
-add - /add channel_link add channel to subscription pool. Channel must have public name. Automatically subscribe this channel if messge is send in a group or channel.
-list - /list: list all channels.
-show_keys - /show_keys: show subscription keywords
-keys - /keys keywords: give a new set of keywords, in json format
-remove - /remove index: remove channel in the pool. Bot admin only.
-
-Feedback / feature requests are welcomed at @dushufenxiang_chat.
-
-Github: https://github.com/gaoyunzhi/telegram_channel_subscription_v2
-
-## bot about 
-
-Subscribe messages from public channels. 
-
-## notes
-
-Part of DB public by design.
+Will add more details if more than 10 people star the repo.
--- setup.py
+++ setup.py
@@ -1,54 +1,19 @@
 import os
 import sys
 
-MAIN_FILE = 'channel_subscription_v2'
-
 def kill():
-	os.system("ps aux | grep ython | grep " + MAIN_FILE + " | awk '{print $2}' | xargs kill -9")
+	os.system("ps aux | grep ython | grep daily_summary | awk '{print $2}' | xargs kill -9")
 
-
-def setup(arg = ''):
-	if arg == 'kill':
-		return kill()
-
-	RUN_COMMAND = 'nohup python3 '+ MAIN_FILE + '.py &'
-
-	if arg != 'debug':
-		r = os.system('sudo pip3 install -r requirements.txt')
-		if r != 0:
-			os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-			os.system('sudo python3 get-pip.py')
-			os.system('rm get-pip.py')
-			os.system('sudo pip3 install -r requirements.txt')
-		
-	try:
-		from telegram.ext import Updater, MessageHandler, Filters
-	except:
-		os.system('sudo pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
-	
-	try:
-		with open('DB') as f:
-			pass
-	except:
-		with open('DB', 'w') as f:
-			f.write("{'pool': []}")
-
-	try:
-		with open('hashes') as f:
-			pass
-	except:
-		with open('hashes', 'w') as f:
-			f.write("!!set\n")
-
+def setup():
 	kill()
-
-	if arg.startswith('debug'):
-		os.system(RUN_COMMAND[6:-2])
+	if 'kill' in str(sys.argv):
+		return 
+	addtional_arg = ' '.join(sys.argv[1:])
+	command = 'python3 -u daily_summary.py %s' % addtional_arg
+	if 'debug' in addtional_arg or 'once' in addtional_arg:
+		os.system(command + ' test')
 	else:
-		os.system(RUN_COMMAND)
+		os.system('nohup %s &' % command)
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		setup(sys.argv[1])
-	else:
-		setup('')
+	setup()
